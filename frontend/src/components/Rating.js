e32750bfc3bb83c96be26caf38a2042d18f6bf7a
shloimiminkowicz <shloimimink@gmail.com>
2020-10-28 21:01:33 -0500

    commit frontend

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 
 const Rating = ({value, text, color}) => {
@@ -32,13 +31,6 @@ const Rating = ({value, text, color}) => {
 
 Rating.defaultProps = {
     color: '#f8e825',
-}
-
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string,
-
 }
 
 export default Rating;
