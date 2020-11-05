0296d7667c2416b5f8fb743e92e30f8b8ee958ed
shloimiminkowicz <shloimimink@gmail.com>
2020-11-05 15:42:50 -0600

    finished product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,20 @@
 import express from 'express';
 
 const router = express.Router()
-import {getProducts, getProductById} from "../controllers/productsController.js";
+import {
+    getProducts,
+    getProductById,
+    deleteProduct,
+    createProduct,
+    updateProduct,
+    createProductReview,
+    getTotProducts
+} from "../controllers/productsController.js";
+import {protect, admin} from "../middleware/authMiddleware.js";
 
-
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTotProducts)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
+import morgan from 'morgan'
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 
 app.use(express.json())
 
@@ -22,7 +30,15 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
+app.get('/api/config/paypal', (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __direname = path.resolve()
+app.use('/uploads', express.static(path.join(__direname, '/uploads')))
 
 app.use(notFound)
 
